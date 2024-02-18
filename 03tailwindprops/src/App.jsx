a5a14d6d0aca512f9cfb4f008e0b77156c3a2561
janak27 <80969920+janak27@users.noreply.github.com>
2024-02-19 00:08:26 +0530

    Bgcolor complete

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,18 +1,50 @@
+import { useState } from "react";
 
-import './App.css'
-import Card from './components/Card'
 function App() {
-  
+  const [color, setColor] = useState("olive");
 
-  let arr = [1,2,3,4];
   return (
-    <>
-      <h1 className=' text-white p-6 border-4 border-slate-200  rounded-2xl my-4'> Tailwind Test</h1>
-      <Card username="janak avhad" somearr = {arr}/>
-      <Card/>
-    </>
+    <div
+      className=" w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className=" fixed flex flex-wrap justify-center bottom-10 inset-x-2 px-3">
+        <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white px-3 py-3 rounded-xl">
+          <button
+          onClick={() => setColor("red")}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
 
-  )
+          <button
+          onClick={() => setColor("blue")}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md"
+            style={{ backgroundColor: "Blue" }}
+          >
+            Blue
+          </button>
+
+          <button
+          onClick={() => setColor("green")}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md"
+            style={{ backgroundColor: "Green" }}
+          >
+            Green
+          </button>
+
+          <button
+          onClick={() => setColor("black")}
+            className="outline-none px-4 py-2 rounded-full text-white shadow-md"
+            style={{ backgroundColor: "Black" }}
+          >
+            Black
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
